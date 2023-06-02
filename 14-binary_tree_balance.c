37d48ee13ff29571908feeaa5e170504afd3ab7f
Brian Kim <bryo.kim1@gmail.com>
2023-06-02 15:30:44 +0300

    Betty Style

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,11 +11,13 @@ size_t binary_tree_height(const binary_tree_t *tree);
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (!tree)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
